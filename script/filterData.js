8a12dc4496ebbce55d01deb7c02504bd03e7cb3f
Guillermo Duque <guille980627@gmail.com>
2023-05-04 21:14:55 -0500

    changes maded

--- script/filterData.js
+++ script/filterData.js
@@ -1,7 +1,7 @@
-import createCards from "../modules/createCards";
+import createCards from "../modules/createCards.js";
 
-async function createChecks() {
-    const url = "https://pro-talento.up.railway.app/api/mindy/products";
+async function createChecks(url) {
+    // const url = "https://pro-talento.up.railway.app/api/mindy/products";
     let response = await fetch(url);
     response = await response.json();
     const categorys = document.getElementById('contenedorCategorias');
@@ -16,14 +16,17 @@ async function createChecks() {
             <label for="${categoria}">${categoria}</label>
             </li>`;
         }
-
     }
-    const checkbox = document.querySelector('input[type="checkbox"]');
-    for (var i = 0; i < checkbox.length; i++)
-        checkbox[i].addEventListener("click", filterType);
-} createChecks("https://pro-talento.up.railway.app/api/mindy/products")
+
+    const checkbox = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkbox.length; i++) {
+        checkbox[i].addEventListener("change", filterType );
+    }
+        
+} createChecks("https://pro-talento.up.railway.app/api/mindy/products");
 
 async function filterType() {
+
     try {
         let text = document.addEventListener("keyup", e => {
             if (e.target.matches("#buscarInput")) {
@@ -34,6 +37,7 @@ async function filterType() {
                 })
             }
         })
+
         let categoria = [...document.querySelectorAll("#filtroCheck:checked")].map(each => each.value).join(",");
         let url = `https://pro-talento.up.railway.app/api/mindy/products/?nombre=${text}&tipo=${categoria}`;
         let response = await fetch(url);
@@ -41,17 +45,18 @@ async function filterType() {
         createCards(response.products);
         console.log()
     } catch (error) {
-console.log(error)
+        console.log(error)
     }
 
 }
 
- document.addEventListener("keyup", e => {
-    if (e.target.matches("#buscarInput")) {
-        document.querySelectorAll(".tCard").forEach(card => {
-            card.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-                ? card.classList.remove("filtro")
-                : card.classList.add("filtro")
-        })
-    }
-})
+
+//  document.addEventListener("keyup", e => {
+//     if (e.target.matches("#buscarInput")) {
+//         document.querySelectorAll(".tCard").forEach(card => {
+//             card.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+//                 ? card.classList.remove("filtro")
+//                 : card.classList.add("filtro")
+//         })
+//     }
+// })
